5a9a04180d3d4795f80b59b2c80777ea5aacd7db
amundskm <amundskm@gmail.com>
2018-08-09 10:03:22 -0500

    adding cheat and guess limiting

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,38 +1,61 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(101)
+    @@guesses = 5
+    SECRET_NUMBER = rand(101)
 
-get '/' do
-    params["guess"] = nil
-    guess = params["guess"]
-    message, background = check_guess(guess, SECRET_NUMBER)
-    erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :background => background}
-end
 
-def check_guess(guess, answer)
-    input = guess.to_i
-    diff = (input - answer)
+    def check_guess(guess, answer)
+        diff = (guess.to_i - answer)
 
-    case
-    when diff <= -5
-        output = "Way too low."
-        color = 'darkred'
-    when (diff > -5) && (diff < 0)
-        output = "Too low."
-        color = 'red'
-    when diff == 0
-        output = "The SECRET NUMBER is #{answer}"
-        color = 'green'
-    when (diff > 0) && (diff < 5)
-        output = "Too high."
-        color = 'red'
-    when diff >= 5
-        output = "Way too high."
-        color = 'darkred'
-    else
-        output = "That is not a valid guess."
+        case
+        when diff <= -5
+            output = "Way too low."
+            color = 'darkred'
+        when (diff > -5) && (diff < 0)
+            output = "Too low."
+            color = 'red'
+        when diff == 0
+            output = "The SECRET NUMBER is #{answer}"
+            color = 'green'
+        when (diff > 0) && (diff < 5)
+            output = "Too high."
+            color = 'red'
+        when diff >= 5
+            output = "Way too high."
+            color = 'darkred'
+        else
+            output = "That is not a valid guess."
+            color = 'orange'
+        end
+
+        return output, color
     end
 
-    return output, color
+get '/' do
+    first_line = "Enter your guess."
+    background = 'white'
+    guess = params["guess"]
+    cheat = ""
+    cheat = "Cheat: The answer is #{SECRET_NUMBER}" if params["cheat"]
+    if guess != nil
+        first_line, background = check_guess(guess, SECRET_NUMBER)
+        @@guesses -= 1  
+        if background == 'green'
+            second_line = "A new number has been chosen"
+            @@guesses = 5
+            SECRET_NUMBER = rand(101)
+            params["guess"] = nil
+        elsif @@guesses == 0
+            first_line = "End of the game, the answer was #{SECRET_NUMBER}"
+            background = 'blue'
+            @@guesses = 5
+            SECRET_NUMBER = rand(101)
+            params["guess"] = nil
+        end  
+        second_line = "You have #{@@guesses} guesses left."
+    end
+     
+
+    erb :index, :locals => {:number => SECRET_NUMBER, :first_line => first_line, :second_line => second_line, :background => background, :cheat => cheat}
 end
